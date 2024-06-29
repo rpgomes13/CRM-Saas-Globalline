82907682bc0ef996099150ea53db29147aa1fb6c
belelemllukas <lukasbelemel@gmail.com>
2024-06-29 14:22:23 -0300

    atualizacao

--- frontend/src/app/components/Charts/page.tsx
+++ frontend/src/app/components/Charts/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import ChartOne from "../../components/Charts/ChartOne";
 import ChartTwo from "../../components/Charts/ChartTwo";
